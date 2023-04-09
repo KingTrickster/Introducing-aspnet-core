bf07809ed8c75b13a2fa0333fdd8c695d5729e9f
Alejandro Trejo <alextrejores@gmail.com>
2023-04-09 00:12:01 -0600

    ADded full oroject

---
+++ ContosoCrafts/ContosoCrafts.Website/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using ContosoCrafts.Website.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoCrafts.Website.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        { this.ProductService = productService; }
+
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get([FromQuery] string productId, [FromQuery] int Rating)
+        { 
+            ProductService.AddRating(productId, Rating);
+            return Ok();
+        }
+    }
+}
+
---
+++ ContosoCrafts/ContosoCrafts.Website/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using ContosoCrafts.Website.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoCrafts.Website.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService;
+        public IEnumerable<Product> Products { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger, JsonFileProductService productService)
+        {
+            ProductService= productService;
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Products = ProductService.GetProducts();
+        }
+    }
+}
